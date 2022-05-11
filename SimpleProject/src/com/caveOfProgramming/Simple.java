b0df7c710117a73ec94b511776016cd76ff49f5f
Navya1680 <knavyareddy1680@gmail.com>
2022-05-11 17:00:54 +0530

    This is First Commit

---
+++ SimpleProject/src/com/caveOfProgramming/Simple.java
@@ -0,0 +1,13 @@
+package com.caveOfProgramming;
+
+public class Simple {
+	private static int a=50;
+	private static  int b=5;
+
+	public static void main(String[] args) {
+	
+		System.out.println(a+b);
+
+	}
+
+}
