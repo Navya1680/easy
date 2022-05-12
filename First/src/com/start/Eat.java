1c0fa535b76a8ad6e2a8c71219f3b27486315b7b
Navya1680 <knavyareddy1680@gmail.com>
2022-05-12 14:34:40 +0530

    Commit Eat to Branch1

---
+++ First/src/com/start/Eat.java
@@ -0,0 +1,14 @@
+package com.start;
+
+public class Eat {
+static String hungry="yes";
+	public static void main(String[] args) {
+		if(hungry=="yes") {
+			System.out.println("Eat something");
+	
+		}else {
+			System.out.println("Do work");
+		}
+	}
+
+}
