6d25ee52e71ca28c0122f018f6ce043f1b1f4f1d
Navya1680 <knavyareddy1680@gmail.com>
2022-05-12 14:28:59 +0530

    Eat Commit to  Branch1

---
+++ SimpleProject/src/com/caveOfProgramming/Eat.java
@@ -0,0 +1,14 @@
+package com.caveOfProgramming;
+
+public class Eat {
+static String hungry="yes";
+	public static void main(String[] args) {
+		if(hungry=="yes") {
+			System.out.println("Eat something");
+	
+		}else {
+			System.out.println("Do work");
+		}
+	}
+
+}
